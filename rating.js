ff1c4e4bfdc894ef62dd362e4abac44f6acd79da
Towobola <towobolak@gmail.com>
2024-03-11 22:40:00 +0100

    final

--- rating.js
+++ rating.js
@@ -1,14 +1,16 @@
-// rating.js
-
+    const selectedRating = document.querySelector('.rating selected').textContent;
+    
 function showRating() {
-    const selectedRating = document.querySelector('.rating .selected').textContent;
-    document.getElementById('selectedRating').textContent = 'You selected ${ratingValue.value} out of 5';
+
+    document.getElementById('selectedRating').textContent = 'You selected ${selectedRating} out of 5';
     document.querySelector('.container1').style.display = 'none';
     document.querySelector('.container2').style.display = 'block';
 }
 
-const numbers = document.querySelectorAll('.rating span');
+    const numbers = document.querySelectorAll('.rating span');
+
 numbers.forEach(number => {
+    
     number.addEventListener('click', () => {
         numbers.forEach(num => num.classList.remove('selected'));
         number.classList.add('selected');
